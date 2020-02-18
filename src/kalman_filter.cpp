8997e8cd00179c5d62e326cfb1210da759d38015
sauravkdeo <saurav.kdeo@gmail.com>
2020-02-18 06:01:08 +0000

    Implemented Predict, Update, UpdateEKF and CommonUpdate functions

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,54 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_*x_;
+  MatrixXd Ft_ = F_.transpose();
+  P_ = F_*P_*Ft_+ Q_;
+  
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+ 
+  VectorXd y_ = z- H_*x_;
+  CommonUpdate(y_);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+  double px = x_(0);
+  double py = x_(1);
+  double vx = x_(2);
+  double vy = x_(3);
+
+  double rho = sqrt(px*px + py*py);
+  double theta = atan2(py, px);
+  double rho_dot = (px*vx + py*vy) / rho;
+  VectorXd h = VectorXd(3);
+  h << rho, theta, rho_dot;
+  VectorXd y = z - h;
+  while ( y(1) > M_PI || y(1) < -M_PI ) {
+    if ( y(1) > M_PI ) {
+      y(1) -= M_PI;
+    } else {
+      y(1) += M_PI;
+    }
+  }
+  CommonUpdate(y);
+}
+
+void KalmanFilter::CommonUpdate(const VectorXd &y){
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+  // New state
+  x_ = x_ + (K * y);
+  int x_size = x_.size();
+  P_ -= K * H_ * P_;
 }
